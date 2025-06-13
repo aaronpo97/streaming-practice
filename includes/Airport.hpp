f16e447e703d451fea5899b573e6e0c33fb0197c
Aaron Po <apo2@uwo.ca>
2025-06-13 02:44:30 -0400

    Update Airport.hpp

--- includes/Airport.hpp
+++ includes/Airport.hpp
@@ -2,6 +2,23 @@
 
 #include <utility>
 
+
+/*
+ *  \name Airport
+ *  \brief Basic data structure to represent an Airport
+ *  \members
+ * - name: Name of the airport.
+ * - province: Province or state where the airport is located.
+ * - city: City where the airport is located.
+ * - iata_code: 3-letter IATA airport code.
+ * - annual_passangers: Number of passengers served annually.
+ * - elevation_ft: Elevation of the airport in feet.
+ * - timezone: Timezone of the airport (e.g., "UTC-5", "Europe/Paris").
+ * - address: Full address of the airport.
+ * - coordinates: Latitude and longitude pair (in degrees).
+*/
+
+
 using Coordinates = std::pair<double, double>;
 
 struct Airport {
